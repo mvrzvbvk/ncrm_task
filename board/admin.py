a71f5eff36543b17a0454bf348f2369c053222cf
dev.myrzabek <dev.myrzabek@gmail.com>
2020-11-29 00:09:25 +0600

    creating models for board app

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Board, Status, Task
+
+
+admin.site.register(Board)
+admin.site.register(Status)
+admin.site.register(Task)
+
+
--- board/models.py
+++ board/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class TrackableDate(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+        
+    class Meta:
+        abstract = True
+
+
+class Board(TrackableDate):
+    board_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Status(TrackableDate):
+    status = models.ForeignKey(Board, related_name="status", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    priority = models.IntegerField(default=0, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(TrackableDate):
+    task = models.ForeignKey(Status, related_name="tasks", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
